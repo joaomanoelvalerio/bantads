5820da1a592d844b695ffa2fd9f480f7a4f9fd80
joaomanoelvalerio <joaomanoelval@gmail.com>
2026-09-08 18:20:55 -0300

    feat: adicionado orquestrador e ms-email com as dependencias necessarias

---
+++ backend/orquestrador/src/test/java/com/br/orquestrador/OrquestradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.orquestrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
